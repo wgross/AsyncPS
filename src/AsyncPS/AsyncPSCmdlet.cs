a939a55bf439098d852180fa52f7e343897121dc
wgross <github@text-mode.de>
2025-12-14 12:34:09 +0100

    initial commit

---
+++ src/AsyncPS/AsyncPSCmdlet.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Concurrent;
+using System.Management.Automation;
+
+namespace AsyncPS;
+
+public class AsyncPSCmdlet : PSCmdlet
+{
+    #region Dispatch into main thread
+
+    private readonly BlockingCollection<Action> dispatcherQueue = new();
+    private readonly int ownerThreadId = Thread.CurrentThread.ManagedThreadId;
+
+    /// <summary>
+    /// Invokes the <paramref name="action"/> in the dispatcher thread
+    /// </summary>
+    public void Dispatch(Action action)
+    {
+        if (this.ownerThreadId == Thread.CurrentThread.ManagedThreadId)
+            action?.Invoke();
+        else if (action is { } notNull)
+            this.dispatcherQueue.Add(notNull);
+    }
+
+    #endregion Dispatch into main thread
+
+    #region Cancel processing records
+
+    /// <summary>
+	/// Cancellation toke source associated to the Ctrl-C event
+	/// </summary>
+    protected CancellationTokenSource ControlCTokenSource { get; } = new();
+
+    /// <summary>
+    /// Propagates Ctrl-C to async operations
+    /// </summary>
+    protected override void StopProcessing() => this.ControlCTokenSource.Cancel();
+
+    protected override void EndProcessing() => this.ControlCTokenSource.Dispose();
+
+    #endregion Cancel processing records
+
+    #region Adapts calls to sync ProcessRecord to async ProcessRecordAsync
+
+    protected override void ProcessRecord()
+    {
+        var cancellationToken = this.ControlCTokenSource.Token;
+        try
+        {
+            // run the async operation from this STA/foreground thread at the thread pool.
+            var processRecordInBackground = Task
+                .Run(async () => await this.ProcessRecordAsync(cancellationToken), cancellationToken)
+                .ContinueWith(t =>
+                {
+                    if(t.IsFaulted)
+                        this.Dispatch(()=>this.WriteError(new ErrorRecord(
+                            exception:t.Exception.InnerException,
+                            errorId:"psasynccmdlet-task-failed",
+                            targetObject:null,
+                            errorCategory:ErrorCategory.OperationStopped)));
+                    this.dispatcherQueue.CompleteAdding();
+                });
+
+            // execute dispatched actions in STA/foreground thread
+            foreach (var action in this.dispatcherQueue.GetConsumingEnumerable(cancellationToken))
+                action?.Invoke();
+
+            // await the finalization of the BG task
+            processRecordInBackground.GetAwaiter().GetResult();
+        }
+        catch (OperationCanceledException)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Override instead of <see cref="ProcessRecord"/> to implement async processing of records
+    /// </summary>
+    protected virtual Task ProcessRecordAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+    #endregion Adapts calls to sync ProcessRecord to async ProcessRecordAsync
+}
---
+++ test/AsyncPS.Test/AsyncPSCmdletTest.cs
@@ -0,0 +1,107 @@
+﻿using System.Diagnostics;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using static Xunit.Assert;
+
+namespace AsyncPS.Test;
+
+public class AsyncPSCmdletTest
+{
+    private readonly PowerShell powershell;
+
+    public AsyncPSCmdletTest()
+    {
+        var sessionState = InitialSessionState.CreateDefault();
+        sessionState.Commands.Add(new SessionStateCmdletEntry("write-object_from_async_code", typeof(write_object_from_async_code_cmdlet), helpFileName: null));
+        sessionState.Commands.Add(new SessionStateCmdletEntry("write-exception_from_async_code", typeof(write_exception_from_async_code_cmdlet), helpFileName: null));
+        sessionState.Commands.Add(new SessionStateCmdletEntry("stop-async_code", typeof(stop_async_code_cmdlet), helpFileName: null));
+        this.powershell = PowerShell.Create(sessionState);
+    }
+
+    #region test cmdlets
+
+    [Cmdlet(VerbsCommunications.Write, "object_from_async_code")]
+    [OutputType(typeof(string))]
+    private sealed class write_object_from_async_code_cmdlet : AsyncPSCmdlet
+    {
+        protected override async Task ProcessRecordAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+
+            this.Dispatch(() => this.WriteObject("1"));
+
+            await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+
+            this.Dispatch(() => this.WriteObject("2"));
+        }
+    }
+
+    [Cmdlet(VerbsCommunications.Write, "exception_from_async_code")]
+    [OutputType(typeof(string))]
+    private sealed class write_exception_from_async_code_cmdlet : AsyncPSCmdlet
+    {
+        protected override async Task ProcessRecordAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+
+            throw new Exception("fail");
+        }
+    }
+
+    [Cmdlet(VerbsLifecycle.Stop, "async_code")]
+    [OutputType(typeof(string))]
+    private sealed class stop_async_code_cmdlet : AsyncPSCmdlet
+    {
+        protected override async Task ProcessRecordAsync(CancellationToken cancellationToken)
+        {
+            cancellationToken.Register(() => this.Dispatch(() => this.WriteObject("stopped")));
+            await Task.Delay(TimeSpan.FromSeconds(10), cancellationToken);
+        }
+    }
+
+    #endregion
+
+    [Fact]
+    public void Write_object_from_async_code()
+    {
+        // ACT
+        var result = this.powershell.AddCommand("write-object_from_async_code").Invoke();
+
+        // ASSERT
+        Equal(["1", "2"], result.Select(o => o.BaseObject));
+    }
+
+    [Fact]
+    public void Write_exception_from_async_code()
+    {
+        // ACT
+        var result = this.powershell.AddCommand("write-exception_from_async_code").Invoke();
+
+        // ASSERT
+        IsType<Exception>(this.powershell.Streams.Error.Single().Exception);
+        Equal("fail", this.powershell.Streams.Error.Single().Exception.Message);
+    }
+
+    [Fact]
+    public async Task Stop_async_code()
+    {
+        // ARRANGE
+        Stopwatch sw = new();
+        sw.Start();
+
+        // cmdlet runs for 10s
+        var psTask = this.powershell.AddCommand("stop-async_code").InvokeAsync();
+
+        await Task.Delay(TimeSpan.FromSeconds(1)); // let cmdlet start processing
+        
+        // ACT
+        this.powershell.Stop();
+        sw.Stop();
+
+        // ASSERT
+        await ThrowsAsync<PipelineStoppedException>(async () => await psTask);
+
+        // stop immediately <<10s
+        True(sw.Elapsed < TimeSpan.FromSeconds(2));
+    }
+}
